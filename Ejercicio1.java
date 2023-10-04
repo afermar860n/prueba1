6277183cdbb4ad5c6e73550c9e7571f8729cd7c3
afermar860n <afermar860n@g.educaand.es>
2023-10-04 10:08:04 +0200

    version inicial

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,2 +1,20 @@
-public class Ejercicio1 {
+import java.util.Scanner;
+
+    public class Ejercicio1 {
+
+        public static void main(String[] args) {
+
+            Scanner entrada = Scanner(System.in);
+
+            double a = entrada.nextDouble();
+            double b = entrada.nextDouble();
+            double c = entrada.nextDouble();
+
+            entrada.close();
+
+            System.out.print(a * b) + (c * (3 - a/(2 *a)));
+
+            System.out.printnl((2 + (a * b) / 4)^ (c + 2))
+
+        }
 }
